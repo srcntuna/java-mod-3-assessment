9cab61d622cfd9485927451581460dda2b19ddc0
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-15 20:21:21 -0400

    progress so far

--- src/main/java/BuilderServices/HospitalSelectionServices.java
+++ src/main/java/BuilderServices/HospitalSelectionServices.java
@@ -3,6 +3,7 @@ package BuilderServices;
 import Entities.Hospital;
 import IOServices.IOService;
 import InputServices.UserInputService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 
@@ -15,6 +16,22 @@ public class HospitalSelectionServices {
     public HospitalSelectionServices(UserInputService userInputService, IOService ioService) {
         this.userInputService = userInputService;
         this.ioService = ioService;
+    }
+
+    public Hospital selectHospital(){
+        System.out.println("WELCOME!");
+        System.out.println("1.Import the existing hospital");
+        System.out.println("2.Create a new hospital");
+        int option = userInputService.getUserIntegerInput("Select an option...");
+
+        Hospital hospital = null;
+        if(option == 1){
+            hospital = importHospital();
+        }else{
+            hospital = buildNewHospital();
+        }
+
+        return hospital;
     }
 
     public Hospital buildNewHospital(){
@@ -37,10 +54,13 @@ public class HospitalSelectionServices {
             restoredHospital = new ObjectMapper().readValue(new File( "hospital.json"), Hospital.class);
 
         } catch (Exception e){
-            e.printStackTrace();
+            System.out.println("Hospital does not exist. Redirecting to create a new one....");
+            return buildNewHospital();
         }
         return restoredHospital;
 
     }
+
+
 
 }
